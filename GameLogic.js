94400ce0bcca5534e05235a10118de4108c047f4
Jonathan Young <jy>
2024-10-31 17:37:56 -0700

    logic

--- GameLogic.js
+++ GameLogic.js
@@ -1,51 +1,46 @@
 class Game {
-  constructor(animateMakeMove, animateWin, displayMessage, pauseAnimation) {
+  constructor(animateMove, animateWin, displayMessage, pauseAnimation) {
     this.board = new Board();
-    this.animateMakeMove = animateMakeMove;
+    this.animateMove = animateMove;
     this.animateWin = animateWin;
     this.displayMessage = displayMessage;
     this.pauseAnimation = pauseAnimation;
 
-    this.waiting = true;
-    this.computerPlayOn = false;
+    this.isWaiting = true;
+    this.isSinglePlayer = false;
     this.computerPlayer = new ComputerPlayerLogic();
   }
 
-  init(isSinglePlayer, p1sym) {
-    this.board.init(p1sym);
-    this.computerPlayOn = isSinglePlayer;
-    if (this.computerPlayOn) {
+  init(isSinglePlayer, player1Symbol) {
+    this.board.init(player1Symbol);
+    this.isSinglePlayer = isSinglePlayer;
+    if (isSinglePlayer) {
       this.computerPlayer.init();
     }
-    this.waiting = true;
+    this.isWaiting = true;
   }
 
   makeMove(index) {
-    index = parseInt(index); //index is the cell of the board
-    if (this.waiting) {
-      if (this.board.getCurrentPlayer() === undefined) {
-        console.error("init() not called yet");
-        return false;
-      }
-      this.waiting = false;
+    index = parseInt(index);
+    if (this.isWaiting) {
+      this.isWaiting = false;
       if (!this._makeMove(index)) {
-        // returns false for invalid move
-        // don't continue if invalid
-        this.waiting = true;
+        this.isWaiting = true;
         return false;
+
       }
 
-      if (this.computerPlayOn) {
-        const wins = this.board.getWinner(); 
-        if (wins.length <= 0 && !this.board.isFull()) {
+      if (this.isSinglePlayer) {
+        const winner = this.board.getWinner();
+        if (winner.length <= 0 && !this.board.isFull()) {
           this.pauseAnimation(() => {
-            const nextMove = this.computerPlayer. getNextMove(index); //waits for animation, then computer moves
+            const nextMove = this.computerPlayer.getNextMove(index);
             this._makeMove(nextMove, true);
-            this.waiting = true; //ready for players move
+            this.isWaiting = true;
           });
         }
       } else {
-        this.waiting = true;
+        this.isWaiting = true;
       }
     }
   }
@@ -54,31 +49,31 @@ class Game {
     return this.board.getGrid();
   }
 
-  currPlayerSymbol() {
-    return this.board.getCurrentPlayer(); // if current player is X or O
+  getCurrentPlayerSymbol() {
+    return this.board.getCurrentPlayer();
   }
 
-// Making a move in the game. 
-
   _makeMove(index, byComputer = false) {
-    const currPlayer = this.board.getCurrentPlayer();
+    const currentPlayer = this.board.getCurrentPlayer();
     const valid = this.board.makeMove(index);
     if (!valid) {
       return false;
     } else {
-      this.animateMakeMove(currPlayer, index);
+      this.animateMove(currentPlayer, index);
     }
+        
+        // returns false for invalid move
+        // don't continue if invalid
 
-    const wins = this.board.getWinner();
-    if (wins.length > 0) { // there is a winner
-      for (const winCombo of wins) {
-        // display winning animation
-        this.animateWin(winCombo.slice(1)); // flash the three winning squares. winCombo is a winning array
+    const winner = this.board.getWinner();
+    if (winner.length > 0) {
+      for (const winCombo of winner) {
+        this.animateWin(winCombo.slice(1));
       }
-      // display winning message with name as argument
       const name = byComputer
         ? "Computer"
-        : (wins[0][0] === this.board.getP1Symbol() ? "Player 1" : "Player 2"); //if winning move is made by computer, name = Computer. If not, gets the winners symvol from the winning aray and checks if its Player1's symbol. If its not, then name = Player 2, if not, name = Player 1
+        : (winner[0][0] === this.board.getPlayer1Symbol() ? "Player 1" : "Player 2");
+        //if winning move is made by computer, name = Computer. If not, gets the winners symvol from the winning aray and checks if its Player1's symbol. If its not, then name = Player 2, if not, name = Player 1
       const message = `${name} won!`;
       this.displayMessage(message);
     } else if (this.board.isFull()) {
@@ -87,3 +82,5 @@ class Game {
     return true;
   }
 }
+
+
--- TicTacToeBoard.js
+++ TicTacToeBoard.js
@@ -1,119 +1,106 @@
-// the cells of the board are represented as an array. Code checks for winning arrays. 
 class Board {
   constructor() {
-    this._rows = 3;
-    this._cols = 3;
-    this._spaces = this._rows * this._cols;
+    this.rows = 3;
+    this.cols = 3;
+    this.totalSpaces = this.rows * this.cols;
     
-    this.grid = Array(this._spaces).fill(null);
-    this.turns = 0;
-    this.wins = [];
-    this.p1 = null;
-    this.p2 = null;
-    this.currPlayer = null;
+    this.grid = Array(this.totalSpaces).fill(null);
+    this.turnCount = 0;
+    this.winCombinations = [];
+    this.player1Symbol = null;
+    this.player2Symbol = null;
+    this.currentPlayer = null;
   }
 
-  init(p1sym) {
-    // Initialize the game state
-    this.turns = 0;
-    this.wins = [];
+  init(player1Symbol) {
+    this.turnCount = 0;
+    this.winCombinations = [];
     this.grid.fill(null);
-    [this.p1, this.p2] = p1sym === 'o' ? ['o', 'x'] : ['x', 'o'];
-    this.currPlayer = this.p1;
+    [this.player1Symbol, this.player2Symbol] = player1Symbol === 'o' ? ['o', 'x'] : ['x', 'o'];
+    this.currentPlayer = this.player1Symbol;
   }
 
   getCurrentPlayer() {
-    return this.currPlayer;
+    return this.currentPlayer;
   }
 
   makeMove(index) {
-    if (this.p1 === null || this.p2 === null) {
-      console.error("init() not called yet");
-      return false;
+    if (!this.player1Symbol || !this.player2Symbol) {
+      throw new Error("init() not called yet");
     }
     if (this.isFull() || this.hasWinner()) {
-      console.error("game over, call init() again");
-      return false;
+      throw new Error("Game over, call init() again");
     }
-    if (index < 0 || index >= this._spaces) {
-      console.error("index should be a number between 0 and 8 (inclusive)");
-      return false;
+    if (index < 0 || index >= this.totalSpaces) {
+      throw new Error("Index should be a number between 0 and 8 (inclusive)");
     }
     if (this.grid[index] !== null) {
-      console.error("grid is already occupied");
-      return false;
+      throw new Error("Cell is already occupied");
     }
-    this.grid[index] = this.currPlayer;
+    this.grid[index] = this.currentPlayer;
     this.updateWinner();
-    this.turns++;
-    this.currPlayer = (this.currPlayer === this.p1) ? this.p2 : this.p1;
+    this.turnCount++;
+    this.currentPlayer = (this.currentPlayer === this.player1Symbol) ? this.player2Symbol : this.player1Symbol;
     return true;
   }
 
   getGrid() {
-    return [...this.grid]; // Return a copy of the grid
+    return [...this.grid];
   }
 
   isFull() {
-    return this.turns >= this._spaces;
+    return this.turnCount >= this.totalSpaces;
   }
 
   getWinner() {
-    return this.wins;
+    return this.winCombinations;
   }
 
-  getP1Symbol() {
-    return this.p1;
+  getPlayer1Symbol() {
+    return this.player1Symbol;
   }
 
   updateWinner() {
-    const rowWin = this.getFullRows();
-    if (rowWin) this.wins.push(rowWin);
+    const rowWin = this.checkRows();
+    if (rowWin) this.winCombinations.push(rowWin);
 
-    const colWin = this.getFullCols();
-    if (colWin) this.wins.push(colWin);
+    const colWin = this.checkCols();
+    if (colWin) this.winCombinations.push(colWin);
 
-    const diagWin = this.getFullDiagonals();
-    if (diagWin) this.wins.push(diagWin);
+    const diagWin = this.checkDiagonals();
+    if (diagWin) this.winCombinations.push(diagWin);
   }
 
   hasWinner() {
-    return this.wins.length > 0;
+    return this.winCombinations.length > 0;
   }
 
-  getFullRows() {
-    for (let i = 0; i < this._rows; i++) {
-      const i1 = i * this._cols;
-      const i2 = i1 + 1;
-      const i3 = i1 + 2;
-      if (this.grid[i1] !== null && this.grid[i1] === this.grid[i2] && this.grid[i2] === this.grid[i3]) {
-        return [this.grid[i1], i1, i2, i3];
+  checkRows() {
+    for (let i = 0; i < this.rows; i++) {
+      const start = i * this.cols;
+      if (this.grid[start] !== null && this.grid[start] === this.grid[start + 1] && this.grid[start + 1] === this.grid[start + 2]) {
+        return [this.grid[start], start, start + 1, start + 2];
       }
     }
     return null;
   }
 
-  getFullCols() {
-    for (let i = 0; i < this._cols; i++) {
-      const i1 = i;
-      const i2 = i + this._cols;
-      const i3 = i + 2 * this._cols;
-      if (this.grid[i1] !== null && this.grid[i1] === this.grid[i2] && this.grid[i2] === this.grid[i3]) {
-        return [this.grid[i1], i1, i2, i3];
+  checkCols() {
+    for (let i = 0; i < this.cols; i++) {
+      if (this.grid[i] !== null && this.grid[i] === this.grid[i + this.cols] && this.grid[i + this.cols] === this.grid[i + 2 * this.cols]) {
+        return [this.grid[i], i, i + this.cols, i + 2 * this.cols];
       }
     }
     return null;
   }
 
-  getFullDiagonals() {
-    // Top-left to bottom-right
-    const diag1 = [0, this._cols + 1, 2 * this._cols + 2];
+  checkDiagonals() {
+    const diag1 = [0, this.cols + 1, 2 * this.cols + 2];
     if (this.grid[diag1[0]] !== null && this.grid[diag1[0]] === this.grid[diag1[1]] && this.grid[diag1[1]] === this.grid[diag1[2]]) {
       return [this.grid[diag1[0]], ...diag1];
     }
 
-    // Top-right to bottom-left
-    const diag2 = [this._cols - 1, 2 * this._cols - 2, 3 * this._cols - 3];
+    const diag2 = [this.cols - 1, 2 * this.cols - 2, 3 * this.cols - 3];
     if (this.grid[diag2[0]] !== null && this.grid[diag2[0]] === this.grid[diag2[1]] && this.grid[diag2[1]] === this.grid[diag2[2]]) {
       return [this.grid[diag2[0]], ...diag2];
     }
@@ -121,3 +108,4 @@ class Board {
     return null;
   }
 }
+
